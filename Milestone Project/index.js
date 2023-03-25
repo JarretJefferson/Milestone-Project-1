84246e685425ea5ddf9cdf965000688a485d4a46
jarretjefferson <jayesper47@gmail.com>
2023-03-25 14:21:55 -0700

    started index.js to move character

--- Milestone Project/index.js
+++ Milestone Project/index.js
@@ -1,5 +1,30 @@
-document.addEventListener("keydown")
+let character = document.getElementById('character')
+character.style.bottom = '515px'
+let move;
 
-function getInput(event){
-    let input = event.key;
-}
+window.addEventListener('keydown',(e)=> {
+    if (e.key === 'w') {
+        move = setInterval(() => {
+            let rect = character.getBoundingClientRect();
+            console.log(window.getComputedStyle(character).getPropertyValue('bottom'))
+            let currentBottom = rect.bottom;
+            let newBottom = currentBottom + 10;
+            character.style.bottom = newBottom + "px";
+        }, 10); // adjust this value to control the speed of movement
+    }
+    if(e.key === 'a') {
+        console.log("foward")
+    }
+    if(e.key === 's') {
+        console.log("foward")
+    }
+    if(e.key === 'd') {
+        console.log("foward")
+       }
+    });
+
+    window.addEventListener('keyup', (e) => {
+        if (e.key === 'w') {
+            clearInterval(move);
+        }
+    });
